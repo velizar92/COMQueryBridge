c2216574b555b5bfd898f0348ccbd548dcf7e88c
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-07-02 21:54:05 +0300

    Set SQLQueryBridge.cs COM class implementation (empty for now just with attributes)

---
+++ COM/COMQueryBridge/SQLQueryBridge.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace COMQueryBridge
+{
+    [ComVisible(true)]
+    [Guid("C53A5A7D-7313-40C4-A628-0C0D4256EFD4")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("COMQueryBridge.SQLQueryBridge")]
+    public class SQLQueryBridge : ISQLQueryBridge
+    {
+        public void Connect(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteNonQuery(string sqlQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteQueryAsJson(string sqlQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExecuteScalar(string sqlQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExportResults()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
